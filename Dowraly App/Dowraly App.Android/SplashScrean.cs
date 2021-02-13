c3ff15ba0463d8710e4655df4751a06d474a8b3d
omar alshobky <93231975za@gmail.com>
2021-02-13 22:04:48 +0200

    add splash screan

---
+++ Dowraly App/Dowraly App.Android/SplashScrean.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dowraly_App.Droid
+{
+    [Activity(Label = "دورللي", Icon = "@drawable/ic_launcher", Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+
+    public class SplashScrean : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+            OverridePendingTransition(0, 0);
+
+                
+        }
+    }
+}
